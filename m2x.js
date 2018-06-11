a8df02eb945044c4d4fa1a2a72032d9b24046754
hohadiwinata <hohadiwinata@connectome.co.nz>
2018-06-11 16:46:35 +1200

    Easily splice the array wanted by typing the number element i want for M2XTrim

--- m2x.js
+++ m2x.js
@@ -3,34 +3,41 @@ var fs = require("fs");
 var rawM2X = fs.readFileSync("M2X.json")
 var M2X = JSON.parse(rawM2X);
 
-M2X50 = M2X.splice(0,50) //Get the first 50 element of the array
+M2XTrim = M2X.splice(0,1000) //Get the first 1000 element of the array
 
-M2X50.forEach(function(soil,index) {
-    currentTimestamp = M2X50[index].timestamp.slice(0,18)
-    currentObject = M2X50[index]
-    nextObject = M2X50[index+1]
-    if (index < M2X50.length - 1) {
-        nextTimestamp = M2X50[index + 1].timestamp.slice(0,18)
+M2XTrim.forEach(function(soil,index) {
+    currentTimestamp = M2XTrim[index].timestamp.slice(0,18)
+    currentObject = M2XTrim[index]
+    nextObject = M2XTrim[index+1]
+    if (index < M2XTrim.length - 1) {
+        nextTimestamp = M2XTrim[index + 1].timestamp.slice(0,18)
         //Find duplicate at certain index number
         if(currentTimestamp == nextTimestamp){ //If there is duplicate
-            console.log("Duplicate found at index: " + index) //print the duplicate at index
+            // console.log("Duplicate found at index: " + index) //print the duplicate at index
             //Begin copying if the value of next object if not empty
             if(nextObject.temperature != '') {
                 currentObject.temperature =  nextObject.temperature
-                console.log("Copied temperature to index: " + nextObject.temperature)
+                // console.log("Copied temperature to index: " + nextObject.temperature)
             }  
             if(nextObject.moisture != '') {
                 currentObject.moisture =  nextObject.moisture
-                console.log("Copied moisture to index: " + nextObject.moisture)
+                // console.log("Copied moisture to index: " + nextObject.moisture)
             }  
             if(nextObject.Battery != '') {
                 currentObject.Battery =  nextObject.Battery
-                console.log("Copied Battery to index: " + nextObject.Battery)
+                // console.log("Copied Battery to index: " + nextObject.Battery)
             }  
-            M2X50.splice(index+1,1) //Remove the next object from array
+            M2XTrim.splice(index+1,1) //Remove the next object from array
         }
     }
     
 })
 
-console.log(M2X50)
+jsonM2X = JSON.stringify(M2XTrim);
+
+// write to file
+fs.writeFile('Merged.json', jsonM2X, 'utf8', function(err) {
+    if(err) console.error(err);
+
+    console.log("File saved successfully")
+});
