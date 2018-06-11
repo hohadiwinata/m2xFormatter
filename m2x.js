61a564a7b125a1329059104e977526b2b77f8063
hohadiwinata <hohadiwinata@connectome.co.nz>
2018-06-11 16:45:42 +1200

    Refactored the flow to copy next object to current object

--- m2x.js
+++ m2x.js
@@ -7,24 +7,27 @@ M2X50 = M2X.splice(0,50) //Get the first 50 element of the array
 
 M2X50.forEach(function(soil,index) {
     currentTimestamp = M2X50[index].timestamp.slice(0,18)
+    currentObject = M2X50[index]
+    nextObject = M2X50[index+1]
     if (index < M2X50.length - 1) {
         nextTimestamp = M2X50[index + 1].timestamp.slice(0,18)
-        //Find duplicate at index number
+        //Find duplicate at certain index number
         if(currentTimestamp == nextTimestamp){ //If there is duplicate
             console.log("Duplicate found at index: " + index) //print the duplicate at index
-            if(M2X50[index+1].temperature != '') {
-                M2X50[index].temperature =  M2X50[index+1].temperature
-                console.log("Copied temperature to index: " + M2X50[index+1].temperature)
+            //Begin copying if the value of next object if not empty
+            if(nextObject.temperature != '') {
+                currentObject.temperature =  nextObject.temperature
+                console.log("Copied temperature to index: " + nextObject.temperature)
             }  
-            if(M2X50[index+1].moisture != '') {
-                M2X50[index].moisture =  M2X50[index+1].moisture
-                console.log("Copied moisture to index: " + M2X50[index+1].moisture)
+            if(nextObject.moisture != '') {
+                currentObject.moisture =  nextObject.moisture
+                console.log("Copied moisture to index: " + nextObject.moisture)
             }  
-            if(M2X50[index+1].Battery != '') {
-                M2X50[index].Battery =  M2X50[index+1].Battery
-                console.log("Copied Battery to index: " + M2X50[index+1].Battery)
+            if(nextObject.Battery != '') {
+                currentObject.Battery =  nextObject.Battery
+                console.log("Copied Battery to index: " + nextObject.Battery)
             }  
-            M2X50.splice(index+1,1)
+            M2X50.splice(index+1,1) //Remove the next object from array
         }
     }
     
